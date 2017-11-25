54828111e59a0d94275f82457093f0f1240f88e6
ViXette <v.xette@gmail.com>
2017-11-26 09:42:40 +1000

    Pipes, Ground

--- Flappy Bird/GameScene.swift
+++ Flappy Bird/GameScene.swift
@@ -12,6 +12,8 @@ class GameScene: SKScene {
 	
 	
 	override func didMove(to view: SKView) {
+		_ = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(self.makePipes), userInfo: nil, repeats: true)
+		
 		/// Background
 		let backgroundTexture = SKTexture(imageNamed: "bg.png")
 		
@@ -30,7 +32,6 @@ class GameScene: SKScene {
 			
 			addChild(backgound)
 		}
-
 		
 		/// Bird
 		let birdTexture = SKTexture(imageNamed: "flappy1.png")
@@ -40,21 +41,56 @@ class GameScene: SKScene {
 		let makeBirdFlap = SKAction.repeatForever(animation)
 		
 		bird = SKSpriteNode(texture: birdTexture)
+
 		bird.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
 		
 		bird.run(makeBirdFlap)
 		
 		self.addChild(bird)
+		
+		/// Ground
+		let ground = SKNode()
+		ground.position = CGPoint(x: self.frame.midX, y: -self.frame.height / 2)
+		ground.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: self.frame.width, height: 1))
+		ground.physicsBody?.isDynamic = false
+		
+		self.addChild(ground)
 	}
 	
 	
 	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		let birdTexture = SKTexture(imageNamed: "flappy1.png")
+		bird.physicsBody = SKPhysicsBody(circleOfRadius: birdTexture.size().height / 2)
 		
+		bird.physicsBody?.isDynamic = true
+		bird.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+		bird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 75))
 	}
 	
 	
 	override func update(_ currentTime: TimeInterval) {
 		
+	}
+	
+	
+	@objc func makePipes () {
+		let movePipes = SKAction.move(by: CGVector(dx: -2 * self.frame.width, dy: 0), duration: TimeInterval(self.frame.width / 100))
+		
+		let gapHeight: CGFloat = bird.size.height * 10
+		let movementAmoint = arc4random() % UInt32(self.frame.height / 2)
+		let pipeOffset = CGFloat(movementAmoint) - self.frame.height / 4
+		
+		let pipe1Texture = SKTexture(imageNamed: "pipe1.png")
+		let pipe1 = SKSpriteNode(texture: pipe1Texture)
+		pipe1.position = CGPoint(x: self.frame.width / 2, y: self.frame.midY + pipe1Texture.size().height / 2 + gapHeight / 2 + pipeOffset)
+		pipe1.run(movePipes)
+		self.addChild(pipe1)
+		
+		let pipe2Texture = SKTexture(imageNamed: "pipe2.png")
+		let pipe2 = SKSpriteNode(texture: pipe2Texture)
+		pipe2.position = CGPoint(x: self.frame.width / 2, y: self.frame.midY - pipe2Texture.size().height / 2 - gapHeight / 2 + pipeOffset)
+		pipe2.run(movePipes)
+		self.addChild(pipe2)
 	}
 	
 }
