effef482102a17a12a5ae9f46ef8fc2ae23ade4c
ViXette <v.xette@gmail.com>
2017-11-25 14:13:56 +1000

    Background

--- Flappy Bird/GameScene.swift
+++ Flappy Bird/GameScene.swift
@@ -8,9 +8,31 @@ import GameplayKit
 class GameScene: SKScene {
 	
 	var bird = SKSpriteNode()
+	var backgound = SKSpriteNode()
 	
 	
 	override func didMove(to view: SKView) {
+		/// Background
+		let backgroundTexture = SKTexture(imageNamed: "bg.png")
+		
+		let moveBackgroundAnimation = SKAction.move(by: CGVector(dx: -backgroundTexture.size().width, dy: 0), duration: 7)
+		let shiftBackgroundAnimation = SKAction.move(by: CGVector(dx: backgroundTexture.size().width, dy: 0), duration: 0)
+
+		for i in 0...2 {
+			backgound = SKSpriteNode(texture: backgroundTexture)
+			
+			backgound.position = CGPoint(x: backgroundTexture.size().width * CGFloat(i), y: self.frame.midY)
+			backgound.size.height = self.frame.height
+			
+			backgound.run(SKAction.repeatForever(SKAction.sequence([moveBackgroundAnimation, shiftBackgroundAnimation])))
+	
+			backgound.zPosition = -1
+			
+			addChild(backgound)
+		}
+
+		
+		/// Bird
 		let birdTexture = SKTexture(imageNamed: "flappy1.png")
 		let birdTexture2 = SKTexture(imageNamed: "flappy2.png")
 		
@@ -18,7 +40,6 @@ class GameScene: SKScene {
 		let makeBirdFlap = SKAction.repeatForever(animation)
 		
 		bird = SKSpriteNode(texture: birdTexture)
-		
 		bird.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
 		
 		bird.run(makeBirdFlap)
